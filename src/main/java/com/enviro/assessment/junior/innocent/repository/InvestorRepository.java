55ccc05d034e1df04cd3d57583d8de6ffbb0f968
SITHEMBISO INNOCENT KUNENE <97756480+InnocentMndexy@users.noreply.github.com>
2026-06-10 15:35:18 +0200

    Delete src directory

--- src/main/java/com/enviro/assessment/junior/innocent/repository/InvestorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enviro.assessment.junior.innocent.repository;
-
-import com.enviro.assessment.junior.innocent.model.Investor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvestorRepository extends JpaRepository<Investor, Long> {
-}
--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO investor (id, first_name, last_name, email, date_of_birth, age)
-VALUES (1, 'Sithembiso', 'Mndebele', 'sthembiso@email.com', '1950-05-15', 75);
-
-INSERT INTO investment_product (id, name, type, balance, investor_id)
-VALUES (1, 'Growth Fund A', 'RETIREMENT', 500000.00, 1),
-       (2, 'Income Fund B', 'SAVINGS', 120000.00, 1);
